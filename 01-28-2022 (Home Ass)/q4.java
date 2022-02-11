c918bcf24351701e5973d9e992e92dbe6bb1ec6b
Ashish17032003 <70490100+Ashish17032003@users.noreply.github.com>
2022-02-11 13:57:31 +0530

    Create q4.java

---
+++ 01-28-2022 (Home Ass)/q4.java
@@ -0,0 +1,25 @@
+//WAP to display the smallest no using 2 inputted no using if else.
+
+import java.util.Scanner;
+public class greatest 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2;
+        System.out.print("Enter three numbers : ");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+      
+        if( n1 < n2)
+            System.out.println(n1 + " is the smaller number.");
+
+        else if (n2 < n1)
+            System.out.println(n2 + " is the smaller number.");
+
+        else
+            System.out.println("Both "+ n1 +"and"+ n2 + "are equal");
+
+        sc.close();
+    }
+}
