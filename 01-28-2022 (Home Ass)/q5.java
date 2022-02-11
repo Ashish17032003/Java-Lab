6fd418c10cc1e266de73ec4531216ef1cc8425e0
Ashish17032003 <70490100+Ashish17032003@users.noreply.github.com>
2022-02-11 13:59:00 +0530

    Create q5.java

---
+++ 01-28-2022 (Home Ass)/q5.java
@@ -0,0 +1,26 @@
+// WAP to display the biggest no from 3 inputted no using if else ladder  and nested if else
+
+
+import java.util.Scanner;
+public class greatest 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1,n2,n3;
+        System.out.print("Enter three numbers : ");
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        n3 = sc.nextInt();
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+
+        sc.close();
+    }
+}
