ef7e0d2371c8579fab3f24ca40599f43ba3a9b66
Ashish17032003 <70490100+Ashish17032003@users.noreply.github.com>
2022-02-11 12:47:17 +0530

    Create q2.java

---
+++ 01-28-22/q2.java
@@ -0,0 +1,36 @@
+//Write a java proogram to find addition , subtraction and multiplication of two numbers
+
+
+import java.util.Scanner;
+public class twonum {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1,num2,c;
+        System.out.print("Enter two numbers : ");
+        num1 = sc.nextInt();
+        num2 = sc.nextInt(); 
+        System.out.println("\n\n1. ADDITION");
+        System.out.println("2. SUBSTRACTION");
+        System.out.println("3. MULTIPLICATION");
+        System.out.println("----Enter your choice----");
+        c = sc.nextInt();
+        switch(c)
+        {
+            case 1: 
+                System.out.println("Addition of "+num1+" and "+num2+" is = " +(num1+num2));
+                break;
+            case 2:
+                System.out.println("Substraction of " + num1 + " and " + num2 + " is = " + (num1 - num2));
+                break;
+            case 3:
+                System.out.println("Multiplication of " + num1 + " and " + num2 + " is = " + (num1*num2));
+
+                break;
+            default:
+                System.out.println("Invalid");
+        }
+
+        sc.close();
+    }
+}
