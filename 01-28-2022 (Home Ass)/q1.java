38ee35e92053e524d9eee241d7f6e5a285be6136
Ashish17032003 <70490100+Ashish17032003@users.noreply.github.com>
2022-02-11 13:00:03 +0530

    Create q1.java

---
+++ 01-28-2022 (Home Ass)/q1.java
@@ -0,0 +1,23 @@
+//WAP to input a no and check whether it is -ve or +ve.
+
+import java.util.Scanner;
+public class greatest 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n1;
+        System.out.print("Enter a number : ");
+        n1 = sc.nextInt();
+        if( n1>0)
+            System.out.println("The number you entered is positive");
+
+        else if (n1<0)
+            System.out.println("The number you entered is negative");
+
+        else
+            System.out.println("The number is neither positive nor negative");
+
+        sc.close();
+    }
+}
